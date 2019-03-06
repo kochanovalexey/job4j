63568de95ff0a8093a9d55ebc9ec1fb9796c3ecb
Kochanov Alexey <dzot-1993@yandex.ru>
2019-03-06 23:49:50 +0300

    1.3. JavaDoc. Code convention[#118467]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Класс выводящий строку "Hello world"
+ * @author akochanov
+ * @since 06.03.2019
+ * @version 1
+*/
 class Calculate {
+	/**
+	* Main
+	* Конструктор, выводящий строку в консоль.
+	* @param arg - arg.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
