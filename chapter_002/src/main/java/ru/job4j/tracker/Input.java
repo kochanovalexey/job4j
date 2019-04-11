bdaa12b7f1f8e0030f59d035633ca965c57df93b
Kochanov Alexey <dzot-1993@yandex.ru>
2019-04-11 21:19:05 +0300

    1. ÐÑÐ¿Ð¾Ð»ÑÐ·ÑÑ ÐºÐ»Ð°ÑÑ ConsoleInput Ð² ÐºÐ»Ð°ÑÑÐµ StartUI Ð¾Ð±ÐµÑÐ¿ÐµÑÐ¸ÑÑ Ð¿Ð¾Ð»Ð½Ð¾ÑÐµÐ½Ð½ÑÑ ÑÐ°Ð±Ð¾ÑÑ Ð²ÑÐµÐ³Ð¾ Ð¿ÑÐ¸Ð»Ð¾Ð¶ÐµÐ½Ð¸Ñ[#118505]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Input.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+/**
+ * Interface Интерфейс Input
+ *
+ * @author akochanov
+ * @version 1
+ * @since 10.04.2019
+ */
+public interface Input {
+    String ask(String question);
+}
