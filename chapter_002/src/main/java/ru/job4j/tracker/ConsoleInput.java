e9b3bb4f0b87d39c04153c3cc2cae1315cc03cfc
Kochanov Alexey <dzot-1993@yandex.ru>
2019-04-17 20:06:48 +0300

    fix ConsoleInput

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -16,6 +16,6 @@ public class ConsoleInput implements Input {
     @Override
     public String ask(String question) {
         System.out.println(question);
-        return scanner.next();
+        return scanner.nextLine();
     }
 }
